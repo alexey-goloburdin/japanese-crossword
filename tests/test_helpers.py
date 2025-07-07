0d58528f89cd1a7fde3a61c1734491ad80a421d2
Alexey Goloburdin <sterx@rl6.ru>
2025-07-07 15:54:43 +0300

    tests refactor

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,33 +1,32 @@
 from japanese_crossword.helpers import find_first_1_index, find_last_1_index
 
 
-def test__find_last_1_index__returns_correct_index_for_complex_input():
-    assert find_last_1_index([None, 1, 1, None, None, 1, 1, 1, 1, 1, None]) == 9
+class TestFindLast1Index:
+    def test_returns_correct_index_for_complex_input(self):
+        assert find_last_1_index([None, 1, 1, None, None, 1, 1, 1, 1, 1, None]) == 9
+
+    def test_returns_correct_index_for_just_one_1(self):
+        assert find_last_1_index([1]) == 0
+
+    def test_returns_correct_index_for_several_1(self):
+        assert find_last_1_index([1, 1, 1, 1, 1]) == 4
+
+    def test_returns_minus_one(self):
+        assert find_last_1_index([None, None]) == -1
 
 
-def test__find_last_1_index__returns_correct_index_for_just_one_1():
-    assert find_last_1_index([1]) == 0
+class TestFindFirst1Index:
+    def test_index_returns_correct_index_for_complex_input(self):
+        assert find_first_1_index([None, 1, 1, None, None, 1]) == 1
 
 
-def test__find_last_1_index__returns_correct_index_for_several_1():
-    assert find_last_1_index([1, 1, 1, 1, 1]) == 4
+    def test_returns_correct_index_for_just_one_1(self):
+        assert find_first_1_index([1]) == 0
 
 
-def test__find_last_1_index__returns_minus_one():
-    assert find_last_1_index([None, None]) == -1
+    def test_returns_correct_index_for_several_1(self):
+        assert find_first_1_index([1, 1, 1]) == 0
 
 
-def test__find_first_1_index__returns_correct_index_for_complex_input():
-    assert find_first_1_index([None, 1, 1, None, None, 1]) == 1
-
-
-def test__find_first_1_index__returns_correct_index_for_just_one_1():
-    assert find_first_1_index([1]) == 0
-
-
-def test__find_first_1_index__returns_correct_index_for_several_1():
-    assert find_first_1_index([1, 1, 1]) == 0
-
-
-def test__find_first_1_index__returns_minus_one():
-    assert find_first_1_index([None, None]) == -1
+    def test_returns_minus_one(self):
+        assert find_first_1_index([None, None]) == -1
